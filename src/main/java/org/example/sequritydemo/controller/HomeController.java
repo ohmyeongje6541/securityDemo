247039c137bcd7aba23d97bf400216b9186d6a33
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-28 13:32:25 +0900

    홈 컨트롤러/홈 뷰 구현

---
+++ src/main/java/org/example/sequritydemo/controller/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.sequritydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/info")
+    public String info(){
+        return "info";
+    }
+}
