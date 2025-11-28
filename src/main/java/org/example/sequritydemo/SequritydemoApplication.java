38fa313e79b5042daf18348236d2747777146750
ohmyeongje <tiqmtiqm6541@gmail.com>
2025-11-28 09:20:36 +0900

    project init

---
+++ src/main/java/org/example/sequritydemo/SequritydemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.sequritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequritydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SequritydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sequritydemo/SequritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sequritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequritydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
